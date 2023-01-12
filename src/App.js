cc4de2f087b474cf5d896e8b031d4dcf1600a922
Emrah Aksoy <emraksoy37@gmail.com>
2023-01-13 00:03:30 +0300

    first delevops

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
 import './App.css';
+import { raceTimerAddress } from "./config";
+import useConnection from "./hooks/useConnection";
+import useContract from "./hooks/useContract";
+import racekronojson from "./metadata/racekrono.json";
+import { useState, useEffect } from "react";
+import { Form, InputGroup, Button, Container, Row, Col, Alert, ListGroup, ListGroupItem, FormControl} from 'react-bootstrap';
+
+
+
 
 function App() {
+  const [currentDate, setCurrentDate] = useState('');
+  const [currentTime, setCurrentTime] = useState('');
+  const [takeTime, setTakeTime] = useState([]);
+  const [signedTime, setSignedTime] = useState({doorNumber:0, time:""});
+
+  const connection = useConnection();
+  const contract = useContract(raceTimerAddress, racekronojson.abi);
+
+
+
+  const flyingFinish = (e) => {
+    setTakeTime((takeTime) => [...takeTime, currentTime]);
+   
+  };
+  
+  
+
+  useEffect(() => {
+
+    let date = new Date().getDate();
+    let month = new Date().getMonth() + 1;
+    let year = new Date().getFullYear();
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ms = new Date().getMilliseconds();
+
+    date = checkTime(date);
+    month = checkTime(month);
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    ms = checkMs(ms);
+
+
+    function checkTime(i) {
+      if (i < 10) { i = "0" + i }
+      return i;
+    }
+
+    function checkMs(x) {
+      if (x < 10) { x = "00" + x }
+      if (x >= 10 && x < 100) { x = "0" + x }
+      return x;
+    }
+
+    setTimeout(() => {
+      setCurrentTime(h + ":" + m + ":" + s + ":" + ms);
+    }, 1);
+
+
+    setCurrentDate(date + "/" + month + "/" + year);
+
+  });
+
+
+
+  const signer = (e) => {
+      setSignedTime(e.target.parentNode.children[0].value, e.target.parentNode.children[1].value);
+      e.target.disabled = true;
+      e.target.nextSibling.disabled = true;
+      e.target.value="Signed";
+      e.target.parentNode.children[0].disabled = true;
+      e.target.parentNode.style.backgroundColor = "yellowgreen";
+
+      console.log(e.target.parentNode.children[1])
+
+    
+      //console.log(e.target.parentNode, currentTime);
+  };
+
+
+  const canceler = (e) => {
+      e.target.disabled = true;
+      e.target.previousSibling.disabled = true;
+      e.target.value="Cancelled";
+      e.target.parentNode.children[0].disabled = true;
+      e.target.parentNode.style.backgroundColor = "pink";
+
+
+      //console.log(e.target.parentNode.children[0].value, e.target.parentNode);     
+  };
+
+
+  
+
+
+  const listItems = takeTime.map((e, index) => {
+    
+
+        return <ListGroupItem key={index} className="mb-1" style={{display:"flex"}} variant='secondary' disabled={false}>
+
+              <FormControl className='item' readOnly={false} placeholder='Door Number' defaultValue={""} type='number'></FormControl>
+
+              <div className='item' style={{backgroundcolor:"blue"}}>{e}</div>
+
+              <Button as="input" readOnly className='item' variant="success" onClick={signer} disabled={false} defaultValue="Sign"></Button>
+            
+              <Button as="input" readOnly className='item' variant="danger" style={{marginRight:'0'}} onClick={canceler} disabled={false} defaultValue="Cancel"></Button>
+        
+        </ListGroupItem>
+    
+  }  
+  );  
+
+ 
+//----------------------------
+  useEffect(() => {
+    connection.connect();
+    //her renderda cüzdanın bağlı olup olmadığının kontrolünü yaptıktan sonra listTasks() fonksiyonu çağırıyoruz 
+    if (connection.address) {
+     // listTimes();
+    }
+  }, [connection.address])
+//--------------------------------
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container className='App sticky-top p-0 mb-3'>    
+      <p className='par p-0'>Flying Finish</p>
+      <p className='par p-0'>{currentDate}</p>
+      <Button onClick={flyingFinish}>{currentTime}</Button>
+    </Container>
+
+    
+    <Container>
+      <Row>
+      <Col md={3}> <h1>COL-1</h1></Col>
+      <Col xs={12} md={6}>
+
+        <ListGroup>
+          {listItems}
+        </ListGroup>
+      </Col>
+      <Col md={3}> <h1>{}</h1></Col>
+      </Row>
+      
+  
+      </Container>
+    </>
+
+    
   );
+
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const raceTimerAddress = "0xDA0bab807633f07f013f94DD0E6A4F96F8742B53"
+ 
